b64d8d2e6543bdff4a60caa0c791460b5c329390
Elena Kashtanova <lenachestnut@gmail.com>
2020-08-08 18:36:32 +0300

    clean up menu panel component

--- src/components/MenuPanel.js
+++ src/components/MenuPanel.js
@@ -1,66 +1,8 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Plus } from 'react-feather';
 import { NewProjectForm } from './Form';
 import ProjectList from './ProjectList';
-
-// class MenuPanel extends Component {
-// 	state = {
-// 		isFormVisible: false,
-// 	};
-
-// 	toggleProjectForm = () => {
-// 		this.setState((prevState) => {
-// 			return {
-// 				isFormVisible: !prevState.isFormVisible,
-// 			};
-// 		});
-// 	};
-
-// 	useEffect(() => {
-// 		if (this.state.isFormVisible && !this.props.isMenuOpen) {
-
-// 		}
-// 	}, [this.props.isMenuOpen]);
-
-// 	render() {
-// 		// Change menu classes when the menu button in navbar is clicked
-// 		let menuClasses = ['menu-panel'];
-// 		if (this.props.isMenuOpen) {
-// 			menuClasses = [...menuClasses, 'open'];
-// 		}
-
-// 		// Check if form should be open
-// 		let projectForm;
-// 		if (this.state.isFormVisible) {
-// 			projectForm = (
-// 				<NewProjectForm toggleProjectForm={this.toggleProjectForm} />
-// 			);
-// 		} else {
-// 			projectForm = (
-// 				<button
-// 					className="new-project-btn"
-// 					onClick={this.toggleProjectForm}
-// 				>
-// 					<Plus />
-// 					<p>New project</p>
-// 				</button>
-// 			);
-// 		}
-
-// 		return (
-// 			<div className={menuClasses.join(' ')}>
-// 				<ul className="projects-container">
-// 					<li className="project-card selected">
-// 						<p>All tasks</p>
-// 					</li>
-// 					<ProjectList projects={this.props.projects} />
-// 				</ul>
-// 				{projectForm}
-// 			</div>
-// 		);
-// 	}
-// }
 
 function MenuPanel(props) {
 	const [isFormVisible, setVisibility] = useState(false);
