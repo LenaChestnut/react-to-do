e904938b91c082a7d2b6102d5c74353257e0cdbd
Elena Kashtanova <lenachestnut@gmail.com>
2020-08-16 12:16:14 +0300

    Make task form component reusable

--- src/components/FormComponents.js
+++ src/components/FormComponents.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { format } from 'date-fns';
 import { Plus, X } from 'react-feather';
 import PropTypes from 'prop-types';
 
@@ -26,4 +27,111 @@ CancelButton.propTypes = {
 	onClick: PropTypes.func,
 };
 
-export { SaveButton, CancelButton };
+function TaskForm(props) {
+	const data = props.props;
+
+	const taskDefault = data.task || {
+		title: '',
+		description: '',
+		project: {
+			id: data.currentProject.id,
+			title: data.currentProject.title,
+		},
+		priority: '3',
+		dueDate: format(Date.now(), 'yyyy-MM-dd'),
+		infoType: 'task',
+	};
+
+	const [task, setTask] = useState(taskDefault);
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		if (name !== 'project') {
+			setTask({ ...task, [name]: value });
+		} else {
+			const { id, title } = JSON.parse(value);
+			console.log(id);
+			console.log(title);
+			setTask({ ...task, project: { id, title } });
+		}
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		data.addTask(task);
+		data.closeForm();
+	};
+
+	return (
+		<form
+			name={data.formName}
+			className="task-form"
+			onSubmit={handleSubmit}
+		>
+			<h2>{data.formHeader}</h2>
+			<input
+				type="text"
+				name="title"
+				value={task.title}
+				placeholder="Task title"
+				required
+				onChange={handleChange}
+			></input>
+			<textarea
+				name="description"
+				value={task.description}
+				placeholder="Task description"
+				required
+				onChange={handleChange}
+			></textarea>
+			<p>Project</p>
+			<select
+				name="project"
+				required
+				value={JSON.stringify(task.project)}
+				onChange={handleChange}
+			>
+				{data.projects.map((project) => {
+					return (
+						<option
+							key={project.id}
+							value={JSON.stringify({
+								id: project.id,
+								title: project.title,
+							})}
+						>
+							{project.title}
+						</option>
+					);
+				})}
+			</select>
+			<p>Priority</p>
+			<select
+				name="priority"
+				value={task.priority}
+				required
+				onChange={handleChange}
+			>
+				<option value="3">Low priority</option>
+				<option value="2">Medium priority</option>
+				<option value="1">High priority</option>
+			</select>
+			<input
+				type="date"
+				name="dueDate"
+				min={format(Date.now(), 'yyyy-MM-dd')}
+				value={task.dueDate}
+				required
+				onChange={handleChange}
+			></input>
+			<SaveButton />
+			<CancelButton onClick={data.closeForm} />
+		</form>
+	);
+}
+
+TaskForm.propTypes = {
+	props: PropTypes.object,
+};
+
+export { SaveButton, CancelButton, TaskForm };
